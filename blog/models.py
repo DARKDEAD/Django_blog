e51d3c47cb3df0fa29aea84ca7d57ba941620047
Dmitriy <komfort1c@gmail.com>
2017-01-17 19:12:16 +0300

    Дописал model

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 
 class Post(models.Model):
     STATUS_CHOICES =(
@@ -28,6 +29,15 @@ class Post(models.Model):
 
     object = models.Manager()
     published = PublishedManager()
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                       self.publish.strftime('%m'),
+                       self.publish.strftime('%d'),
+                       self.slug]
+                       )
+
 class Meta:
     ordering = ('-publish',)
 
